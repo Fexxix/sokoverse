e89bd21f41fd7cee3a25bccc901b2cc7d74b1a06
Fexxix <fexxix@gmail.com>
2025-04-16 20:18:40 +0500

    whoops forgot this

---
+++ lib/client/wasm/hmac.ts
@@ -0,0 +1,15 @@
+import init, { hmac_sign } from "@/pkg/wasm-hmac/wasm_hmac"
+
+let wasm: Awaited<ReturnType<typeof init>>
+
+async function initWasmIfNeeded() {
+  if (!wasm) {
+    wasm = await init()
+  }
+  return wasm
+}
+
+export async function hmacSign(payload: string) {
+  await initWasmIfNeeded()
+  return hmac_sign(payload)
+}
