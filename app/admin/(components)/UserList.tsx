66e65198b1d9d4be0b76ddefec9b99aad26f6040
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-06-28 15:55:45 +0500

    feat: Users Page

---
+++ app/admin/(components)/UserList.tsx
@@ -0,0 +1,42 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { UserType } from "./UsersPage";
+import UserCard from "./UserCard";
+
+interface UsersListProps {
+  users: UserType[];
+  getTotalLevels: (user: UserType) => number;
+  setSelectedUser: (user: UserType) => void;
+  selectedUser: UserType | null;
+}
+
+export default function UsersList({
+  users,
+  getTotalLevels,
+  setSelectedUser,
+  selectedUser,
+}: UsersListProps) {
+  if (users.length === 0) {
+    return (
+      <Card className="bg-white border border-gray-200 rounded-lg shadow-sm">
+        <CardContent className="py-8 flex justify-center items-center">
+          <p className="text-gray-400 text-sm">
+            No users found matching your search.
+          </p>
+        </CardContent>
+      </Card>
+    );
+  }
+  return (
+    <>
+      {users.map((user) => (
+        <UserCard
+          key={user.id}
+          user={user}
+          getTotalLevels={getTotalLevels}
+          setSelectedUser={setSelectedUser}
+          selectedUser={selectedUser}
+        />
+      ))}
+    </>
+  );
+}
